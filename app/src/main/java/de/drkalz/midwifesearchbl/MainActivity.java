7c48802aef50162ea976bc7580129d155345e0a6
AlexanderKalz <Alexander.Kalz@gmx.de>
2016-02-24 21:46:34 +0100

    v0.01

---
+++ app/src/main/java/de/drkalz/midwifesearchbl/MainActivity.java
@@ -0,0 +1,66 @@
+package de.drkalz.midwifesearchbl;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final String APP_KEY = "FB722433-CE7D-F7A1-FF00-4A5A61834900";
+    public final String API_KEY = "5E881F51-12A2-A115-FFA1-C2120C3B0B00";
+    public final String APP_VERSION ="v1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Backendless.initApp(APP_KEY, API_KEY, APP_VERSION);
+        BackendlessUser currentUser = Backendless.UserService.CurrentUser();
+
+        if (currentUser == null) {
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
